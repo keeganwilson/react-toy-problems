a271cb1e47649cb1f6050f3628041f882e6e0dc0
Keegan Wilson <keegan.wilson@gmail.com>
2022-05-23 18:59:48 -0600

    completed

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Palindrome = () => {
-  return <p className="palindrome">Palindrome</p>;
+  const [userInput, setUserInput] = useState("");
+  const [palindrome, setPalindrome] = useState("");
+
+  const isPalindrome = (props) => {
+    let result = "";
+    let reversed = props.split("").reverse().join("");
+    if (props === reversed) {
+      result = "Yes";
+    } else result = "No";
+    setPalindrome(result);
+  };
+
+  const inputChangeHandler = (event) => {
+    setUserInput(event.target.value);
+  };
+
+  const submitHandler = () => {
+    isPalindrome(userInput);
+  };
+
+  return (
+    <div className="puzzleBox filterStringPB">
+      <h4>Palindrome</h4>
+      <input className="inputLine" onChange={inputChangeHandler}></input>
+      <button className="confirmationButton" onClick={submitHandler}>
+        Submit
+      </button>
+      <span className="resultsBox">Is it a palindrome? {palindrome}</span>
+    </div>
+  );
 };
 
 export default Palindrome;
--- src/components/Topics/Sum.js
+++ src/components/Topics/Sum.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Sum = () => {
-  return <p className="sum">Sum</p>;
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [sum, setSum] = useState(null);
+
+  const add = (num1, num2) => {
+    let sum = +num1 + +num2;
+    setSum(sum);
+  };
+
+  const inputChangeHandler1 = (event) => {
+    setNumber1(event.target.value);
+  };
+
+  const inputChangeHandler2 = (event) => {
+    setNumber2(event.target.value);
+  };
+
+  const submitHandler = () => {
+    add(number1, number2);
+  };
+
+  return (
+    <div className="puzzleBox sumPB">
+      <h4>Sum</h4>
+      <input className="inputLine" onChange={inputChangeHandler1}></input>
+      <input className="inputLine" onChange={inputChangeHandler2}></input>
+      <button className="confirmationButton" onClick={submitHandler}>
+        Submit
+      </button>
+      <span className="resultsBox">Result: {sum}</span>
+    </div>
+  );
 };
 
 export default Sum;
